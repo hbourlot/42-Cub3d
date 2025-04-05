99e64df101e4b066c2097dbb6e9e3514cdddce50
Jorge Marquez <jorgemiliano91@gmail.com>
2025-04-05 11:15:06 +0100

    Remake: Build from a prev commit, and remove unusefull stuff since beggining

--- inc/definitions.h
+++ inc/definitions.h
@@ -6,7 +6,7 @@
 /*   By: joralves <joralves@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 09:19:52 by hbourlot          #+#    #+#             */
-/*   Updated: 2025/04/01 01:15:57 by joralves         ###   ########.fr       */
+/*   Updated: 2025/04/05 11:14:10 by joralves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@
 #define IMG_HEIGHT 256
 #define MAP_HEIGHT 768
 #define MAP_WIDTH 1024
-#define VPL "NSWE" // valid player location
+#define VPL "NSWE"            // valid player location
 #define IPL " \t\n\v\f\r\a\b" // Ingnore player location
 
 #define PI 3.1415926535
@@ -31,22 +31,11 @@
 #define P3 3 * PI / 2
 #define DR 0.0174533 // one degree in radians
 
-// SCREEN VARS
-#define SCREEN_SCALE 3
-#define SCREEN_WIDTH 320 * SCREEN_SCALE
-#define SCREEN_HEIGHT 200 * SCREEN_SCALE
+#define SCREEN_WIDTH 900
+#define SCREEN_HEIGHT 600
 
-// PLANE VARS
-#define GAME_SCALE 2 * SCREEN_SCALE
-#define GAME_WIDTH SCREEN_WIDTH / GAME_SCALE
-#define GAME_HEIGHT SCREEN_HEIGHT / GAME_SCALE
-
-// OTHER PLANE VARS
 #define FOV PI / 3
-#define CANT_RAYS GAME_WIDTH / 4
-#define ANGLE_PER_RAY  FOV / CANT_RAYS
-#define RENDER_LINE_WIDTH GAME_WIDTH / CANT_RAYS
-#define HORIZON  GAME_HEIGHT / 2
+
 
 #define SPEED 2.0
 #define ROT_SPEED DR * 5
@@ -54,7 +43,6 @@
 // world vars
 #define TILE_SIZE 16
 #define WALL_SIZE 16
-#define PLAYER_SIZE WALL_SIZE - 2
 
 // others
 #define EPSILON 0.000001
--- src/draw/dda.c
+++ src/draw/dda.c
@@ -6,7 +6,7 @@
 /*   By: joralves <joralves@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 15:01:42 by joralves          #+#    #+#             */
-/*   Updated: 2025/04/01 16:27:45 by joralves         ###   ########.fr       */
+/*   Updated: 2025/04/05 11:14:05 by joralves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,7 +117,7 @@ void	render_walls(t_cub3d *game, t_raycast *raycast, int color)
 		while (y < raycast->wall_height)
 		{
 			raycast->screen_y = raycast->wall_start + y;
-			if (raycast->screen_y >= 0 && raycast->screen_y < GAME_HEIGHT)
+			if (raycast->screen_y >= 0 && raycast->screen_y < SCREEN_HEIGHT)
 				my_mlx_pixel_put(game, x, raycast->screen_y, color);
 			y++;
 		}
@@ -132,23 +132,23 @@ void	cast_render_raycast(t_cub3d *game)
 	int			color;
 
 	i = 0;
-	raycast.column_width = (int)((float)GAME_WIDTH / (float)CANT_RAYS);
+	raycast.column_width = (int)((float)SCREEN_WIDTH / (float)SCREEN_WIDTH);
 	if (raycast.column_width < 1)
 		raycast.column_width = 1;
-	while (i < CANT_RAYS)
+	while (i < SCREEN_WIDTH)
 	{
-		raycast.alpha = FOV / 2 - ANGLE_PER_RAY * i;
+		raycast.alpha = FOV / 2 - FOV/SCREEN_WIDTH * i;
 		raycast.ray_angle = game->player.angle + raycast.alpha;
 		normalize_angle(&raycast.ray_angle);
 		dda(game, &raycast, game->player.x, game->player.y);
 		raycast.perp_dist = raycast.dist * cos(raycast.alpha);
-		raycast.wall_height = (int)(WALL_SIZE * GAME_HEIGHT
+		raycast.wall_height = (int)(WALL_SIZE * SCREEN_HEIGHT
 				/ raycast.perp_dist);
-		raycast.wall_start = GAME_HEIGHT / 2 - raycast.wall_height / 2;
+		raycast.wall_start = SCREEN_HEIGHT / 2 - raycast.wall_height / 2;
 		raycast.x_start = i * raycast.column_width;
 		raycast.x_end = (i + 1) * raycast.column_width;
-		if (raycast.x_end > GAME_WIDTH)
-			raycast.x_end = GAME_WIDTH;
+		if (raycast.x_end > SCREEN_WIDTH)
+			raycast.x_end = SCREEN_WIDTH;
 		if (raycast.dda.hitside == 1)
 			color = create_rgb(0, 255, 0, 0);
 		else
