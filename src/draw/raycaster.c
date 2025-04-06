362a463b91302e94ad1d679bd857b2779fb1f99d
Jorge Marquez <jorgemiliano91@gmail.com>
2025-04-06 15:33:48 +0100

    Remake: Remove unused variable, makint it more readable

--- src/draw/raycaster.c
+++ src/draw/raycaster.c
@@ -2,20 +2,20 @@
 
 #include "cub3d.h"
 
-void	render_walls(t_cub3d *game, t_raycast *raycast, int color)
+void	render_walls(t_cub3d *game, t_raycast *ray, int color)
 {
 	int	x;
 	int	y;
 
-	x = raycast->x_start;
-	while (x < raycast->x_end)
+	x = ray->x_start;
+	while (x < ray->x_end)
 	{
 		y = 0;
-		while (y < raycast->wall_height)
+		while (y < ray->wall_height)
 		{
-			raycast->screen_y = raycast->wall_start + y;
-			if (raycast->screen_y >= 0 && raycast->screen_y < SCREEN_HEIGHT)
-				my_mlx_pixel_put(game, x, raycast->screen_y, color);
+			ray->screen_y = ray->wall_start + y;
+			if (ray->screen_y >= 0 && ray->screen_y < SCREEN_HEIGHT)
+				my_mlx_pixel_put(game, x, ray->screen_y, color);
 			y++;
 		}
 		x++;
@@ -24,33 +24,28 @@ void	render_walls(t_cub3d *game, t_raycast *raycast, int color)
 
 void	cast_render_raycast(t_cub3d *game)
 {
-	t_raycast raycast;
+	t_raycast ray;
 	int i;
 	int color;
 
 	i = 0;
-	raycast.column_width = (int)((float)SCREEN_WIDTH / (float)SCREEN_WIDTH);
-	if (raycast.column_width < 1)
-		raycast.column_width = 1;
 	while (i < SCREEN_WIDTH)
 	{
-		raycast.alpha = FOV / 2 - FOV / SCREEN_WIDTH * i;
-		raycast.ray_angle = game->player.angle + raycast.alpha;
-		normalize_angle(&raycast.ray_angle);
-		dda(game, &raycast, game->player.x, game->player.y);
-		raycast.perp_dist = raycast.dist * cos(raycast.alpha);
-		raycast.wall_height = (int)(WALL_SIZE * SCREEN_HEIGHT
-				/ raycast.perp_dist);
-		raycast.wall_start = SCREEN_HEIGHT / 2 - raycast.wall_height / 2;
-		raycast.x_start = i * raycast.column_width;
-		raycast.x_end = (i + 1) * raycast.column_width;
-		if (raycast.x_end > SCREEN_WIDTH)
-			raycast.x_end = SCREEN_WIDTH;
-		if (raycast.dda.hitside == 1)
+		ray.alpha = FOV / 2 - FOV / SCREEN_WIDTH * i;
+		ray.ray_angle = game->player.angle + ray.alpha;
+		normalize_angle(&ray.ray_angle);
+		dda(game, &ray, game->player.x, game->player.y);
+		ray.perp_dist = ray.dist * cos(ray.alpha);
+		ray.wall_height = (int)(WALL_SIZE * SCREEN_HEIGHT
+				/ ray.perp_dist);
+		ray.wall_start = SCREEN_HEIGHT / 2 - ray.wall_height / 2;
+		ray.x_start = i;
+		ray.x_end = i + 1;
+		if (ray.dda.hitside == 1)
 			color = create_rgb(0, 255, 0, 0);
 		else
 			color = create_rgb(0, 0, 0, 255);
-		render_walls(game, &raycast, color);
+		render_walls(game, &ray, color);
 		i++;
 	}
 }
